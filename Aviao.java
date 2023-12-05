2035e30a4c0cad3b0c38471015400b9007476d3c
otaviohiratsuka <132589540+otaviohiratsuka@users.noreply.github.com>
2023-12-05 15:25:22 -0300

    Add files via upload

---
+++ Aviao.java
@@ -0,0 +1,26 @@
+public class Aviao {
+
+    private int numeroPassageiro;
+    private String companhiaAerea;
+    private int ID;
+    
+    public int getNumeroPassageiro() {
+        return numeroPassageiro;
+    }
+    public void setNumeroPassageiro(int numeroPassageiro) {
+        this.numeroPassageiro = numeroPassageiro;
+    }
+    public String getCompanhiaAerea() {
+        return companhiaAerea;
+    }
+    public void setCompanhiaAerea(String companhiaAerea) {
+        this.companhiaAerea = companhiaAerea;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    
+}
