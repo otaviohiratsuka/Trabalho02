a337c73002be0efae12bbc3b1b2c15af81f45121
ANDERSON RODRIGUES DOS SANTOS <60968822+AndersonR-S@users.noreply.github.com>
2023-12-06 09:00:26 -0300

    Aviao atualizado

--- Aviao.java
+++ Aviao.java
@@ -1,25 +1,33 @@
 public class Aviao {
 
-    private int numeroPassageiro;
-    private String companhiaAerea;
-    private int ID;
+    private int numero_passageiro;
+    private String companhia_aerea;
+    private String ID;
+
+    public Aviao(){}
     
+    public Aviao(String id, int numero_passageiro, String companhia_aerea){
+        this.ID = id;
+        this.numero_passageiro = numero_passageiro;
+        this.companhia_aerea = companhia_aerea;
+    }   
+
     public int getNumeroPassageiro() {
-        return numeroPassageiro;
+        return numero_passageiro;
     }
-    public void setNumeroPassageiro(int numeroPassageiro) {
-        this.numeroPassageiro = numeroPassageiro;
+    public void setNumeroPassageiro(int numero_passageiro) {
+        this.numero_passageiro = numero_passageiro;
     }
     public String getCompanhiaAerea() {
-        return companhiaAerea;
+        return companhia_aerea;
     }
-    public void setCompanhiaAerea(String companhiaAerea) {
-        this.companhiaAerea = companhiaAerea;
+    public void setCompanhiaAerea(String companhia_aerea) {
+        this.companhia_aerea = companhia_aerea;
     }
-    public int getID() {
+    public String getID() {
         return ID;
     }
-    public void setID(int iD) {
+    public String setID(String iD) {
         ID = iD;
     }
     
